f600528cf6120366b6f422d68f63e4c223e7f9ff
exmyth <iexmyth@gmail.com>
2016-01-21 21:27:17 +0800

    save to database when username is offline

--- src/org/androidpn/server/xmpp/push/NotificationManager.java
+++ src/org/androidpn/server/xmpp/push/NotificationManager.java
@@ -17,8 +17,15 @@
  */
 package org.androidpn.server.xmpp.push;
 
+import java.util.List;
 import java.util.Random;
 
+import org.androidpn.server.model.Notification;
+import org.androidpn.server.model.User;
+import org.androidpn.server.service.NotificationService;
+import org.androidpn.server.service.ServiceLocator;
+import org.androidpn.server.service.UserNotFoundException;
+import org.androidpn.server.service.UserService;
 import org.androidpn.server.xmpp.session.ClientSession;
 import org.androidpn.server.xmpp.session.SessionManager;
 import org.apache.commons.logging.Log;
@@ -40,12 +47,18 @@ public class NotificationManager {
     private final Log log = LogFactory.getLog(getClass());
 
     private SessionManager sessionManager;
+    
+    private UserService userService;
+    
+    private NotificationService notificationService;
 
     /**
      * Constructor.
      */
     public NotificationManager() {
         sessionManager = SessionManager.getInstance();
+        userService = ServiceLocator.getUserService();
+        notificationService = ServiceLocator.getNotificationService();
     }
 
     /**
@@ -58,13 +71,27 @@ public class NotificationManager {
      */
     public void sendBroadcast(String apiKey, String title, String message,
             String uri) {
-        log.debug("sendBroadcast()...");
+    	log.debug("sendBroadcast()...");
+        /*
         IQ notificationIQ = createNotificationIQ(apiKey, title, message, uri);
         for (ClientSession session : sessionManager.getSessions()) {
             if (session.getPresence().isAvailable()) {
                 notificationIQ.setTo(session.getAddress());
                 session.deliver(notificationIQ);
             }
+        }
+        */
+        IQ notificationIQ = createNotificationIQ(apiKey, title, message, uri);
+        List<User> users = userService.getUsers();
+        for(User user : users){
+        	ClientSession session = sessionManager.getSession(user.getUsername());
+        	if(session != null && session.getPresence().isAvailable()){
+        		notificationIQ.setTo(session.getAddress());
+                session.deliver(notificationIQ);
+        	}
+        	else{
+        		saveNotification(apiKey, user.getUsername(), title, message, uri);
+        	}
         }
     }
 
@@ -86,7 +113,31 @@ public class NotificationManager {
                 notificationIQ.setTo(session.getAddress());
                 session.deliver(notificationIQ);
             }
+            else{
+            	saveNotification(apiKey, username, title, message, uri);
+            }
         }
+        else{
+        	try {
+				User user = userService.getUserByUsername(username);
+				if(user != null){
+					saveNotification(apiKey, username, title, message, uri);
+				}
+			} catch (UserNotFoundException e) {
+				e.printStackTrace();
+			}
+        }
+    }
+    
+    private void saveNotification(String apiKey, String username,
+            String title, String message, String uri){
+    	Notification notification = new Notification();
+    	notification.setApiKey(apiKey);
+    	notification.setUsername(username);
+    	notification.setTitle(title);
+    	notification.setMessage(message);
+    	notification.setUri(uri);
+    	notificationService.saveNotification(notification);
     }
 
     /**
